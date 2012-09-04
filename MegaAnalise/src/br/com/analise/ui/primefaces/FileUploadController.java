e3ddd1ab1ff73db85a02db317e75c54ed3933fb3
Y81W <Y81W@MI00253722.petrobras.biz>
2012-09-04 16:46:56 -0300

    alteraçõess

--- MegaAnalise/src/br/com/analise/ui/primefaces/FileUploadController.java
+++ MegaAnalise/src/br/com/analise/ui/primefaces/FileUploadController.java
@@ -1,5 +1,8 @@
 package br.com.analise.ui.primefaces;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
@@ -8,9 +11,24 @@ import org.primefaces.event.FileUploadEvent;
 public class FileUploadController {
 	
 
-	   public void handleFileUpload(FileUploadEvent event) {  
-	        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
+	private InputStream fileUploaded;
+	
+		
+	public InputStream getFileUploaded() {
+		return fileUploaded;
+	}
+
+	public void setFileUploaded(InputStream fileUploaded) {
+		this.fileUploaded = fileUploaded;
+	}
+
+
+
+	public void handleFileUpload(FileUploadEvent event) throws IOException {  
+	        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
 	        FacesContext.getCurrentInstance().addMessage(null, msg);  
+	        
+	        this.fileUploaded =  event.getFile().getInputstream();	        
 	    }  
 	
 }
