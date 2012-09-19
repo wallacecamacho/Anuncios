8799dfa980f6b9fa22bdff32d587fe362e953211
Y81W <Y81W@MI00253722.petrobras.biz>
2012-09-19 17:59:46 -0300

    sd

--- MegaAnalise/src/br/com/analise/algorithm/CombAnalise.java
+++ MegaAnalise/src/br/com/analise/algorithm/CombAnalise.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.jboss.solder.logging.Logger;
@@ -30,16 +31,52 @@ public class CombAnalise {
 	
 	public void verificaCombinacoes(){
 		
-		List<Object>  teste = templateNativeQuery.getCountSorteios();
+		//Quantidade de sorteios realizados
+		List<Object>  qtdSorteios = templateNativeQuery.getCountSorteios();
 		
-		for(Object e : teste ){
+		for(Object e : qtdSorteios ){
 			
 			logger.info("Sorteio de numero = " + e.toString());
-			logger.info("List de numero do sorteio = " + e.toString() + "  -  " + buscaNumerosdoSorteio((Integer)e).size() );
+			//logger.info("List de numero do sorteio = " + e.toString() + "  -  " + buscaNumerosdoSorteio((Integer)e).size() );
 			
+			//Busca os números do sorteio i
+			 List<NumeroSorteado> numerosSort = buscaNumerosdoSorteio((Integer)e);
 			
+			 //Para cada número do sorteio
+			for(NumeroSorteado num : numerosSort ){
+				
+				logger.info("numero sorteado = " + num.getNumeroSorteado());
+				
+				//Consulta todos os sorteios que contem o numero de i
+				//for(int i=0; i<=numerosSort.size()-1; i++){
+					
+					//Consulta todos os numeros do sorteio que contem o numero de i 
+					List<NumeroSorteado> contemNumero = buscaNumeros(num.getNumeroSorteado());
+					
+					//
+					for(NumeroSorteado sort: contemNumero){
+					for(NumeroSorteado num2 : numerosSort ){
+						
+							
+							if(num2.getNumeroSorteado()==sort.getNumeroSorteado() ){
+								
+								logger.info("numero sorteado é igual = " + num.getNumeroSorteado());
+								logger.info("Sorteio a comparar = " + num2.getIdNumeroSorteado());
+								logger.info("Sorteio comparado = " + sort.getIdNumeroSorteado());
+								
+							}
+							
+						}
+					
+				}
+				
+			//}
+			
+			}
 			
 		}
+			
+		
 		
 	}
 	
@@ -55,6 +92,22 @@ public class CombAnalise {
 		//Predicate condi = criteriaBuilder.equal(numeroSorteado.get("desctTipo"), pDescricao);
 		
 		criteriaQuery.select(numeroSorteado).where(criteriaBuilder.equal(path, pISorteio));
+		
+		return em.createQuery(criteriaQuery).getResultList();	
+
+	}
+	
+	public List<NumeroSorteado> buscaNumeros(Integer pNum) {
+
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<NumeroSorteado> criteriaQuery = criteriaBuilder.createQuery(NumeroSorteado.class);
+		Root<NumeroSorteado> numeroSorteado = criteriaQuery.from(NumeroSorteado.class);
+		//Path<Object> path = numeroSorteado.join("sorteio");
+		//ParameterExpression<Integer> param = criteriaBuilder.parameter(Integer.class);
+		
+		Predicate condi = criteriaBuilder.equal(numeroSorteado.get("numeroSorteado"), pNum);
+		
+		criteriaQuery.select(numeroSorteado).where(condi);
 		
 		return em.createQuery(criteriaQuery).getResultList();	
 
--- MegaAnalise/src/br/com/analise/bean/query/TemplateQuery.java
+++ MegaAnalise/src/br/com/analise/bean/query/TemplateQuery.java
@@ -16,14 +16,5 @@ public interface TemplateQuery extends Serializable{
 	 @Query("select t from Sorteio t")
 	   public List<Sorteio> getAllSorteios();
 	 
-	 @Query("select t.dezena1 dez,t.idSorteio   from Sorteio t  " +
-	 		"union select t.dezena2 dez,idSorteio  from Sorteio t " +
-	 		"union select t.dezena3 dez,idSorteio  from Sorteio t" +
-	 		"union select t.dezena4 dez,idSorteio  from Sorteio t" +
-	 		"union select t.dezena5 dez,idSorteio  from Sorteio t" +
-	 		"union select t.dezena6 dez,idSorteio  from Sorteio t ")
-	   public List<Sorteio> getAllSorteiosG();
-	
-
 
 }
